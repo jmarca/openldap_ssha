e8c561739ef232396637803f30514b9490861919
James E. Marca <james@activimetrics.com>
2020-04-06 10:07:47 -0700

    update run docker

--- run_docker.sh
+++ run_docker.sh
@@ -62,6 +62,56 @@ relies_on_network(){
     done
 }
 
+relies_on_volume(){
+    for volume in "$@"; do
+        local state
+        state=$(docker volume inspect --format "{{.CreatedAt}}" "$volume" 2>/dev/null)
+
+        if [[ "$state" == "false" ]] || [[ "$state" == "" ]]; then
+            echo "$volume is not up, starting it for you."
+            docker volume create $volume
+        fi
+    done
+}
+
+openldap_nw(){
+    docker network create --driver bridge openldap_nw
+}
+
+
+openldap(){
+    relies_on_network openldap_nw
+    relies_on_volume ldap_etc_ldap ldap_ssl ldap_var_backups ldap_var_lib_ldap ldap_var_restore
+
+    docker run --rm -d \
+           --network openldap_nw \
+           --name openldap \
+           --mount source=ldap_etc_ldap,target="/etc/ldap" \
+           --mount source=ldap_ssl,target="/ssl" \
+           --mount source=ldap_var_backups,target="/var/backups" \
+           --mount source=ldap_var_lib_ldap,target="/var/lib/ldap" \
+           --mount source=ldap_var_restore,target="/var/restore" \
+           -e DOMAIN="activimetrics.com" \
+           -e ORGANIZATION="Activimetrics LLC" \
+           -e PASSWORD="grobblefruit" \
+           mwaeckerlin/openldap
+}
+
+openldapam(){
+    relies_on openldap
+    relies_on_network openldap_nw
+    relies_on_volume ldap_etc_ldap ldap_ssl ldap_var_backups ldap_var_lib_ldap ldap_var_restore
+    docker run --rm -d \
+           --name lam \
+           --network openldap_nw \
+           --mount source=ldap_etc_ldap,target="/etc/ldap" \
+           --mount source=ldap_ssl,target="/ssl" \
+           --mount source=ldap_var_backups,target="/var/backups" \
+           --mount source=ldap_var_lib_ldap,target="/var/lib/ldap" \
+           --mount source=ldap_var_restore,target="/var/restore" \
+           mwaeckerlin/lam
+}
+
 openldap_ssha_sh(){
     del_stopped "openldap_ssha"
     docker run --rm -it -v ${PWD}:/usr/src/dev  --name openldap_ssha jmarca/openldap_ssha sh
